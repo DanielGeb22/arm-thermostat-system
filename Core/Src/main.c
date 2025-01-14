d5146a5b5bbb62d6a9db60fa849d83ab727e40ac
Daniel Gebran <danielgebran92@gmail.com>
2025-01-13 17:08:29 -0800

    Added sensor Read and Write functions

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -114,8 +114,57 @@ uint8_t DS18B20_Init(void)
 		response = -1;
 	}
 
-	delay(400);
+	delay(400);		// 80 + 400 = 480us delay
 	return response;
+}
+
+void DS18B20_Write(uint8_t data)
+{
+	set_pin_output(DS18B20_PORT, DS18B20_PIN);
+
+	for (int i = 0; i < 8; i++)
+	{
+		if ((data & (1<<i)) != 0) // if i-th bit is high, write 1
+		{
+			set_pin_output(DS18B20_PORT, DS18B20_PIN);
+			HAL_GPIO_WritePin(DS18B20_PORT, DS18B20_PIN, 0);
+			delay(1);	// pull low for 1us
+
+			set_pin_input(DS18B20_PORT, DS18B20_PIN);
+			delay(60);	// then release pin for 60us
+		}
+		else // if i-th bit is low, write 0
+		{
+			set_pin_output(DS18B20_PORT, DS18B20_PIN);
+			HAL_GPIO_WritePin(DS18B20_PORT, DS18B20_PIN, 0);
+			delay(60);	// pull low for 60us
+
+			set_pin_input(DS18B20_PORT, DS18B20_PIN);	// then release pin
+		}
+	}
+}
+
+uint8_t DS18B20_Read(void)
+{
+	uint8_t value = 0;
+
+	set_pin_input(DS18B20_PORT, DS18B20_PIN);
+
+	for (int i = 0; i < 8; i++)
+	{
+		set_pin_output(DS18B20_PORT, DS18B20_PIN);
+
+		HAL_GPIO_WritePin(DS18B20_PORT, DS18B20_PIN, 0);
+		delay(2);
+
+		set_pin_input(DS18B20_PORT, DS18B20_PIN);
+		if (HAL_GPIO_ReadPin(DS18B20_PORT, DS18B20_PIN)) // if pin is high, read 1
+		{
+			value |= 1<<i;
+		}
+		delay(60);
+	}
+	return value;
 }
 
 
