a36c42a134aad9eda6fcad571deccd6aa3b4a589
Daniel Gebran <danielgebran92@gmail.com>
2025-01-31 19:41:56 -0800

    Finalized servo functionality to turn off AC

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -75,7 +75,7 @@ uint16_t SUM, RH, TEMP;
 
 float temperature = 0;
 uint8_t presence = 0;
-int pwmValue = 450;
+int pwmValue = 2500;
 
 void set_pin_output(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
 {
@@ -239,18 +239,23 @@ int main(void)
 
 	HAL_Delay(3000);
 
-	while (pwmValue > 500) {
+	while (pwmValue > 500) {	// Move arm to the right at constant speed
 		pwmValue -= 5;
 		__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, pwmValue);
 		HAL_Delay(2);
 	}
 
-	while (pwmValue > 370) {
-		pwmValue -= 5;
+	while (pwmValue > 370) {	// Once button is reached, hold down for 4 seconds
+		pwmValue -= 5;			// to turn off AC by gradually pushing arm down
 		__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, pwmValue);
-		HAL_Delay(200);
+		if (pwmValue > 400) {
+			HAL_Delay(200);
+		}
+		else {
+			HAL_Delay(500);
+		}
 	}
-	while (pwmValue < 2500) {
+	while (pwmValue < 2500) {	// Arm goes back to initial state after button press
 		pwmValue += 5;
 		__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, pwmValue);
 		HAL_Delay(2);
